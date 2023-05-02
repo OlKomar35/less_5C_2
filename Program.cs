7bc1d5991f44611c77142ba16c8d5f25efaf099a
Olga Komar <komar_ol@mail.ru>
2023-05-02 11:25:17 +0300

    added the solution to the second task

--- Program.cs
+++ Program.cs
@@ -3,3 +3,43 @@
 
 //[3, 7, 23, 12] -> 19
 //[-4, -6, 89, 6] -> 0
+
+int[] CreateArray(int lengthArray, int start, int finish){
+    int [] array = new int[lengthArray];
+    for(int i=0; i<lengthArray;i++){
+        array[i] = new Random().Next(start,finish+1);
+    }
+return array;
+}
+
+void SumOfElementsWithOddIndex(int [] array){
+    int sum = 0;
+    for (int index = 0; index < array.Length; index++)
+    {
+        if(index % 2 == 0 ) sum+=array[index];
+    }
+    Console.WriteLine($"Сумма чисел с нечетным индексом = {sum}");
+}
+
+void PrintArray(int [] array){
+    Console.Write("[");
+    for(int i=0;i<array.Length;i++){
+        if(i!=array.Length-1)
+            Console.Write($"{array[i]}, ");
+        else
+            Console.WriteLine($"{array[i]}]");
+    }
+}
+
+Console.Write("Введите длину массива N=");
+int lengthArr = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите начало диапозона min=");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите окончание диапазона max=");
+int max = Convert.ToInt32(Console.ReadLine());
+
+
+int[] arr = CreateArray(lengthArr,min,max);
+PrintArray(arr);
+SumOfElementsWithOddIndex(arr);
+
